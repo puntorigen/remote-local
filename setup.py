34618cf327d558bf25d37833e2154a87d7867553
Pablo Schaffner <pablo@puntorigen.com>
2025-04-26 11:05:09 -0400

    - version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="remote-local",
-    version="0.0.1",
+    version="0.0.2",
     description="Expose your local FastAPI, NiceGUI, or development servers remotely — easily, reliably, and for free.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
